f5b56df96e6fe10ac3df160d52f473239d74de63
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-03-19 14:33:39 -0700

    Working app v1.0

--- index.js
+++ index.js
@@ -1,96 +1,84 @@
-//Calorie count back in 
-//Toggle the show more-recipes button - have it load after the recipes have loaded
-//Styling the show-more-recipes button - 
-//Refactor macronutrient calculator 
-//Make sure errors are taken care of 
-//Style app - responsive, font, color, and images
-//Commit to Git 
-//Submit an MVP 
-//Get feedback and submit in a Gist
-//Fix accessibility - check axe 
-//Documentation README
-//Comment code to help
-
 'use strict';
-//Helper Functions 
 
-var searchTerm; 
+var searchTerm;
 
 function handleSubmitButton() {
     $('#search-form').submit(function (e) {
         searchTerm = '';
+        content = '';
         e.preventDefault();
-        recognition.stop();
         searchTerm = $('#text-area').val();
         $('#text-area').val('');
         parseRequest(searchTerm);
     });
 }
 
-function parseRequest(request){
+function parseRequest(request) {
     request = request.toLowerCase();
-    if(request.includes('recipe')){
+    if (request.includes('recipe')) {
+        if ($('#show-more-recipes').is(':visible')) {
+            toggleShowMoreRecipes();
+        };
         clearResults();
-        recipeStart = 10;
+        recipeStart = 12;
         getRecipeData(request);
-    } else if (request.includes('analyze')){
-        obj = {};
-        if ($("#show-more-recipes").is(":visible")) {
+    } else if (request.includes('analyze')) {
+        nutrientCount = {};
+        if ($('#show-more-recipes').is(':visible')) {
             toggleShowMoreRecipes();
         };
         clearResults();
         getNutritionData(request);
     } else {
-        $('#results').text("I'm sorry I don't know how to do that. Try asking me to analyze or search for recipes.")
+        if ($('#show-more-recipes').is(':visible')) {
+            toggleShowMoreRecipes();
+        };
+        apiError();
     }
 }
 
-function clearResults(){
-    $("#results").empty();
+function removeInstructions() {
+    $('#remove-instructions').on('click', function (e) {
+        $('#instructions').remove();
+    });
 }
 
-function toggleShowMoreRecipes(){
-    $("#show-more-recipes").toggle();
+function clearResults() {
+    $('#results').empty();
 }
 
-$('#show-more-recipes').on('click', function(e){
-    e.preventDefault();
-    recipeStart = recipeStart + 10; 
-    getRecipeData(searchTerm)
-})
-//handle show more button function 
-
-//API Calls 
-
-//Analyze Nutrition 
+/*--------------------API Calls---------------------*/
 
 function getNutritionData(searchTerm) {
     $.ajax({
-        url: `https://trackapi.nutritionix.com/v2/natural/nutrients`,
+        url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
         type: 'post',
         headers: {
-            "x-app-id": "e4b7e33f",
-            "x-app-key": "c95b8bbb3aa85b4f2f37ec0bab80dd4e",
+            'x-app-id': 'e4b7e33f',
+            'x-app-key': 'c95b8bbb3aa85b4f2f37ec0bab80dd4e',
         },
         data: {
-            "query": searchTerm,
+            'query': searchTerm,
         },
         success: function (data) {
-            console.log(data);
-            const foods = data.foods
-            // calculateCalories(foods);
-            calculateMacronutrients(foods);
-            calculateMicronutrients(foods);
-           
+            const foods = data.foods;
+
+            if (foods.length === 0) {
+                noResultsError();
+            } else {
+                calculateMacronutrients(foods);
+                calculateMicronutrients(foods);
+                calculateCalories(foods);
+            }
+        },
+        error: function () {
+            apiError();
         }
-    })
+    });
 }
 
-//Recipes 
 
-var recipeStart; 
-
-function getRecipeData(searchTerm){
+function getRecipeData(searchTerm) {
     let url = 'https://api.edamam.com/search';
     let id = '54f6a76e';
     let key = 'ed9fa62cc29a1ae5e51dff6c1f623e40';
@@ -101,128 +89,145 @@ function getRecipeData(searchTerm){
         app_key: key
     };
     $.getJSON(url, query, function (data) {
-        console.log(data);
-        const results = data.hits
-        console.log(results)
-        renderRecipes(results);
+        const results = data.hits;
+        if (results.length === 0) {
+            noResultsError();
+        } else {
+            renderRecipes(results);
+        }
     });
 }
 
-// //Render Functions 
+/*--------------------Error Functions---------------------*/
 
-function renderRecipes(results){
-    
+function noResultsError() {
+    $('#error').toggle().html(`
+    <div id="error-container">
+    <p>No results. Try checking your spelling or rewording your request and resubmitting.  If that doesn't work try a different request.<a href="#" id="empty-error"> [X]</a></p>
+    </div>
+    `);
+    emptyError();
+}
+
+function apiError() {
+    $('#error').toggle().html(`
+    <div id="error-container">
+    <p>No results. Try checking your spelling or rewording your request and resubmitting. If that doesn't work try a different request.<a href="#" id="empty-error"> [X]</a></p>
+    </div>
+    `);
+    emptyError();
+}
+
+function emptyError() {
+    $('#empty-error').on('click', function (e) {
+        $('#error').empty().toggle();
+    });
+}
+
+/*--------------------Recipe Functions---------------------*/
+
+var recipeStart;
+
+function toggleShowMoreRecipes() {
+    $('#show-more-recipes').toggle();
+}
+
+function handleShowMoreRecipesButton() {
+    $('#show-more-recipes').on('click', function (e) {
+        e.preventDefault();
+        recipeStart = recipeStart + 12;
+        getRecipeData(searchTerm);
+    });
+}
+
+function renderRecipes(results) {
     let recipes = results.map(result => {
         return {
             image: result.recipe.image,
             title: result.recipe.label,
-            url: result.recipe.url 
+            url: result.recipe.url
         }
     })
-
     let recipeList = recipes.map(recipe => {
         return `
-        <div class = "recipes col-4">
-            <a href="${recipe.url}"><img src="${recipe.image}"></a>
-            <p>${recipe.title}</p>
+        <div class ="recipe-card">
+            <a target="_blank" href="${recipe.url}"><img class="recipe-card-image"src="${recipe.image}" alt="${searchTerm}-${recipe.title}">
+            <p class="recipe-card-title">${recipe.title}</p></a>
         </div>
-        `})
+        `});
     $('#results').html(recipeList)
-
-    if (!$("#show-more-recipes").is(":visible")){
+    if (!$("#show-more-recipes").is(":visible")) {
         toggleShowMoreRecipes();
+        handleShowMoreRecipesButton();
     };
 }
 
-// //Nutrient Calculations 
 
-// function calculateCalories(foods){
-//     const calories = Math.round(foods.map(food => food.nf_calories).reduce((a,b) => a+b))
-//     makeCaloriesChart(calories);
-// }
+/*--------------------Nutrition Analysis Functions---------------------*/
 
 const NUTRIENTS_ATTR_ID = [291, 301, 304, 307, 309, 320, 323, 401];
 const NUTRIENTS_RDA = [25, 1000, 380, 2000, 10, 800, 15, 80];
-var obj = {};
+var nutrientCount = {};
 
-function calculateMicronutrients(foods){
+function calculateCalories(foods) {
+    const calories = Math.round(foods.map(food => food.nf_calories).reduce((a, b) => a + b));
+    renderCalories(calories);
+}
+
+function calculateMicronutrients(foods) {
     foods.forEach(food => {
         NUTRIENTS_ATTR_ID.forEach(id => {
             var nutrient = food.full_nutrients.find(nutrient => {
                 return nutrient.attr_id === id;
             });
             if (nutrient) {
-                consolidate(nutrient);
+                consolidateNutrientData(nutrient);
             }
         });
-    })
-    const nutrientTotals = Object.values(obj)
-    console.log(nutrientTotals);
-    const micronutrientPercents = nutrientTotals.map((nutrientTotal,index) => Math.round(nutrientTotal/NUTRIENTS_RDA[index] * 100))
+    });
+    const nutrientTotals = Object.values(nutrientCount);
+    const micronutrientPercents = nutrientTotals.map((nutrientTotal, index) => Math.round(nutrientTotal / NUTRIENTS_RDA[index] * 100));
     makeMicrosChart(micronutrientPercents);
 }
 
-function consolidate(item) {
-    if (obj.hasOwnProperty(item.attr_id)) {
-        obj[item.attr_id] += item.value;
-    } else {
-        obj[item.attr_id] = item.value;
-    }
-}
-
-function calculateMacronutrients(foods){
+function calculateMacronutrients(foods) {
     let macronutrients = [];
-    macronutrients.push(Math.round(foods.map(food => food.nf_protein).reduce((a, b) => a + b)))
-    macronutrients.push(Math.round(foods.map(food => food.nf_total_fat).reduce((a, b) => a + b)))
-    macronutrients.push(Math.round(foods.map(food => food.nf_total_carbohydrate).reduce((a, b) => a + b)))
+    macronutrients.push(Math.round(foods.map(food => food.nf_protein).reduce((a, b) => a + b)));
+    macronutrients.push(Math.round(foods.map(food => food.nf_total_fat).reduce((a, b) => a + b)));
+    macronutrients.push(Math.round(foods.map(food => food.nf_total_carbohydrate).reduce((a, b) => a + b)));
     makeMacrosChart(macronutrients);
 }
 
-
-//Speech Recognition
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.continuous = true;
-
-const textArea = $('#text-area');
-var content = '';
-
-recognition.onresult = function (event) {
-    var current = event.resultIndex;
-    var transcript = event.results[current][0].transcript;
-    content += transcript;
-    textArea.val(content);
-};
-
-recognition.onstart = function () {
-    console.log('Started Listening');
-};
-
-recognition.onspeechend = function () {
-    console.log('Stopped listening');
-};
-
-
-$('#start-btn').on('click', function (e) {
-    if (content.length) {
-        content += ' ';
+function consolidateNutrientData(item) {
+    if (nutrientCount.hasOwnProperty(item.attr_id)) {
+        nutrientCount[item.attr_id] += item.value;
+    } else {
+        nutrientCount[item.attr_id] = item.value;
     }
-    recognition.start();
-    console.log('Started');
-});
-
-// //Chart Functions
-
-function renewCanvas(){
-    $('canvas').remove();
-    $('#results').html(`
-        <canvas id="micros" width="600" height="400"></canvas>
-        <canvas id="macros" width="600" height="400"></canvas>
-    `)
 }
 
-function makeMacrosChart(macronutrients){
+function renderCalories(calories) {
+    $("#calories").text(`${calories} calories`)
+}
+
+/*--------------------Nutrition Chart Functions---------------------*/
+
+function renewCanvas() {
+    $('canvas').remove();
+    $('#results').html(` 
+      <div id="micros-chart-card">
+        <canvas id="micros" width="300" height="200"></canvas>
+      </div>
+      <div id="macros-chart-card">
+       <canvas id="macros" width="300" height="200"></canvas>
+      </div>
+    `);
+    $('#results').prepend(`
+        <div id="calories-card"><div id="calories"></div></div>
+    `);
+}
+
+function makeMacrosChart(macronutrients) {
     renewCanvas();
     var macros = $("#macros");
     var macrosChart = new Chart(macros, {
@@ -237,6 +242,11 @@ function makeMacrosChart(macronutrients){
             ]
         },
         options: {
+            title: {
+                display: true,
+                text: 'Macronutrients(in grams)',
+                fontSize: 16
+            },
             elements: {
                 arc: {
                     borderWidth: 0
@@ -246,40 +256,27 @@ function makeMacrosChart(macronutrients){
     });
 }
 
-function makeMicrosChart(micronutrients){
+function makeMicrosChart(micronutrients) {
     var micros = $("#micros");
     var microsChart = new Chart(micros, {
         type: 'bar',
         data: {
             labels: ['Fiber', 'Calcium', 'Magnesium', 'Sodium', 'Zinc', 'Vit A', 'Vit E', 'Vit C'],
             datasets: [{
-                label: 'Percentage',
+                label: 'Percent of RDA',
                 data: micronutrients,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                    '#FF6384',
+                    '#8087ee',
+                    '#4bb3f2',
+                    '#6cd67e',
+                    '#e7e265',
+                    '#ec9255',
+                    '#425cdb',
+                    '#ea4343',
+                    '#45cebc',
+                    '#e144d2',
+                ]
             }]
         },
         options: {
@@ -289,20 +286,61 @@ function makeMicrosChart(micronutrients){
                         min: 0,
                         max: 100,
                         callback: function (value) { return value + "%" }
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Percentage"
                     }
                 }]
+            },
+            title: {
+                display: true,
+                text: 'Micronutrients(% of RDA)',
+                fontSize: 16
             }
         }
     });
 }
 
+/*--------------------Speech Recognition Functions---------------------*/
 
+function noSpeechRecogntion() {
+    $('#speech-btn').hide();
+    $('#stop-speech-btn').hide();
+    $('#instructions').html(`
+  <p id="instructions">Want to know the nutrition of a meal? Ask me to analyze the ingredients. Don't know what to cook, why not ask me for recipes? For nutrition analysis type "Analyze 1 cup of rice and 2 eggs" -always include the "analyze" keyword followed by the quantity and ingredient. For recipes simply input the ingredient with the keyword "recipes" for example try Sweet potato recipes".  Oh by the way if you open me up in the Chrome you can speak your requests. <a href="#" id="remove-instructions">Remove instructions</a></p> 
+  `);
+}
 
-$(handleSubmitButton)
+function handleStopSpeechButton() {
+    $('#stop-speech-btn').on('click', function () {
+        recognition.stop();
+    });
+}
 
+if (!('webkitSpeechRecognition' in window)) {
+    noSpeechRecogntion();
+} else {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
 
+    const textArea = $('#text-area');
+    var content = '';
 
+    recognition.onresult = function (event) {
+        var current = event.resultIndex;
+        var transcript = event.results[current][0].transcript;
+        content += transcript;
+        textArea.val(content);
+    };
+    $('#speech-btn').on('click', function (e) {
+        if (content.length) {
+            content += ' ';
+        }
+        recognition.start();
+    });
+}
+
+function start() {
+    handleSubmitButton();
+    removeInstructions();
+}
+
+$(start)
